dacf9fcd5270dcd7dbea44fc402cbee930df3fbf
2391134843 <2391134843@qq.com>
2018-07-06 20:06:35 +0800

    add Loading... To Show buffer

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,16 +1,24 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 import { Article } from './article';
 import { ARTICLES } from './mock-article';
 import { Observable, of } from 'rxjs';
 
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+// @Injectable()
+// export class ConfigService {
+  
+// }
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class ArticleService {
-
-  constructor() { }
+constructor(private http: HttpClient) { }
+  
 
   getArticles():Observable<Article[]>{
     return of (ARTICLES);
